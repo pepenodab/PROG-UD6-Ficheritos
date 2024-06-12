8cf6592fa451f6d43f106691fc31ce197da7225a
Marco Gil Flores <usr3886@salesianos-lacuesta.net>
2024-06-12 13:42:31 +0100

    feat(activitytwo):  Acitivy two was finished
    Activity two that concist in print for console a text with the letters separately with downbars and acompanied with the ascii code.

--- src/net/salesianos/actividaddos/App.java
+++ src/net/salesianos/actividaddos/App.java
@@ -1,5 +1,25 @@
 package net.salesianos.actividaddos;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class App {
 
+    public static void main(String[] args) {
+
+        String path = "./src/net/salesianos/actividaddos/output/testazo.txt";
+        
+        try {
+            BufferedReader leerFicheraso = new BufferedReader(new FileReader(path));
+            int asciiNumber = leerFicheraso.read();
+            while (asciiNumber != -1) {
+                System.out.print(((char)asciiNumber + "__" + (asciiNumber) + ", "));
+                asciiNumber = leerFicheraso.read();
+            }
+            leerFicheraso.close();
+        } catch (IOException e) {
+            System.out.println("ta mal");
+        }
+    }
 }
